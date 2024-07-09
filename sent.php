fc42c6b74d9a755debdf7f972e48afe06145e27b
“kaki-moto” <kakimoto182@gmail.com>
2024-07-09 14:05:51 +0900

    Initial commit

---
+++ sent.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>会員登録画面フォーム</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+
+  <body>
+
+    <?php echo $_POST['family']; ?>
+    <?php echo $_POST['first']; ?>
+    <?php echo $_POST['radio']; ?>
+    <?php echo $_POST['pref']; ?>
+    <?php echo $_POST['adress']; ?>
+    <?php echo $_POST['password']; ?>
+    <?php echo $_POST['mail']; ?>
+
+    
+    
+</body>
+
+</html>
