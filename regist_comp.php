f4e14ff00eb63ea39df1ec9733c17f6c93536c7f
“kaki-moto” <kakimoto182@gmail.com>
2024-07-10 22:55:26 +0900

    Create regist_comp.php

---
+++ regist_comp.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>会員登録完了画面</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+
+  <body>
+
+  <div class="form-title">会員登録完了</div>
+  <div>会員登録が完了しました。</div>
+
+</body>
+
+</html>
